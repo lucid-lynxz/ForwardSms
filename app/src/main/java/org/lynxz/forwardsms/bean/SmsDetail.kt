bcd6dc1a01e1d28d7269e25f488d98880ba946c1
lynxz <lynxz8866@gmail.com>
2019-11-10 11:16:03 +0800

    初始值非空

--- app/src/main/java/org/lynxz/forwardsms/bean/SmsDetail.kt
+++ app/src/main/java/org/lynxz/forwardsms/bean/SmsDetail.kt
@@ -7,10 +7,10 @@ import java.text.SimpleDateFormat
 
 data class SmsDetail(
     var ts: Long = System.currentTimeMillis(),// 收到短信的时间戳
-    var from: String? = null,// 原始发信人
+    var from: String? = "",// 原始发信人
     var to: String? = SmsConstantParas.phoneTag, // 短信接收人
-    var displayFrom: String? = null,// 显示的发信人信息
-    var body: String? = null, // 短信内容
+    var displayFrom: String? = "",// 显示的发信人信息
+    var body: String? = "", // 短信内容
     var status: Int = 0, //  短信状态 -1:接收 0:complete 64:pending 128:failed
     var type: Int = 1, // 1:接收的 2:发出的
     var read: Int = 0 // 0:未读 1:已读
@@ -36,7 +36,6 @@ data class SmsDetail(
         } else {
             ""
         }
-
 
         val receiver = if (to.isNullOrBlank()) {
             ""
